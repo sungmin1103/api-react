f902ae633d0d5a3c2f22a7f1051311b299143357
sungmin <kimsungmin2580@gmail.com>
2025-09-05 17:56:58 +0900

    0905 작업

--- src/App.js
+++ src/App.js
@@ -1,64 +1,106 @@
 
-import './App.css';
+// import './App.css';
+// import React, {useEffect, useState} from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Route, Routes, useLocation } from 'react-router-dom';
+// import Home from './pages/Home';
+// import Detail from './pages/Detail';
+// import List from './pages/List';
+
+// export const stateContext = React.createContext();
+
+// const serviceKey="9bd628172007ec6004775ad78add84b28405597ff013237898bf9c2c38c24a55";
+
+// const busanUrl = `http://apis.data.go.kr/6260000/RecommendedService/getRecommendedKr?serviceKey=${serviceKey}&pageNo=8&numOfRows=10&resultType=json`;
+
+// const gyeongnamUrl = `http://apis.data.go.kr/6480000/gyeongnamtourculture/gyeongnamtourculturelist?serviceKey=${serviceKey}&pageNo=5&numOfRows=8&resultType=json`;
+// const App = () => {
+//   const [isDataLoaded, setIsDataLoaded] = useState(false);
+//   const [result, setResult] = useState([]);
+
+//   const [isRowsLoaded, setIsRowsLoaded] = useState(false);
+//   const [rows, setRows] = useState([]);
+
+//   const location = useLocation();
+
+//   useEffect(() => {     
+//     fetch(busanUrl)
+//     .then(response => response.json())
+//     .then(data => {
+//       //console.log(data.getRecommendedKr.item.map((it) => it.MAIN_TITLE));
+//       setResult(data.getRecommendedKr.item);
+//       setIsDataLoaded(true);
+//     })
+//     .catch(error => console.log(error));
+
+//     fetch(gyeongnamUrl)
+//     .then(response => response.json())
+//     .then(data => {
+//       //console.log(data.gyeongnamtourculturelist.body.items.item.map((it) => it.data_title));
+//       setRows(data.gyeongnamtourculturelist.body.items.item);
+//       setIsRowsLoaded(true);
+//     })
+//     .catch(error => console.log(error)); 
+//   },[]);
+//   const value=location.pathname==="/list"?rows:result;
+
+//   if(!isDataLoaded && !isRowsLoaded){
+//     return <div>데이터를 불러오는 중입니다...</div>
+//   }
+//   else{
+//   return (
+//     <>
+//         <stateContext.Provider value={value}>
+//           <Routes>
+//             <Route path='/' element={<Home />} />
+//             <Route path='/detail/:UC_SEQ' element={<Detail />} />
+//             <Route path='/list' element={<List />} />
+//           </Routes>
+//           </stateContext.Provider>
+//       </>
+//   );
+//   };
+// };
+// export default App;
+
 import React, {useEffect, useState} from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
-import Detail from './pages/Detail';
-import List from './pages/List';
 
-export const stateContext = React.createContext();
+const url = 'http://localhost:8080/api/board/list';
+export const BoardStateContext = React.createContext();
 
-const serviceKey="9bd628172007ec6004775ad78add84b28405597ff013237898bf9c2c38c24a55";
-
-const busanUrl = `http://apis.data.go.kr/6260000/RecommendedService/getRecommendedKr?serviceKey=${serviceKey}&pageNo=8&numOfRows=10&resultType=json`;
-
-const gyeongnamUrl = `http://apis.data.go.kr/6480000/gyeongnamtourculture/gyeongnamtourculturelist?serviceKey=${serviceKey}&pageNo=5&numOfRows=8&resultType=json`;
 const App = () => {
+
   const [isDataLoaded, setIsDataLoaded] = useState(false);
   const [result, setResult] = useState([]);
 
-  const [isRowsLoaded, setIsRowsLoaded] = useState(false);
-  const [rows, setRows] = useState([]);
-
-  const location = useLocation();
-
   useEffect(() => {     
-    fetch(busanUrl)
+    fetch(url)
     .then(response => response.json())
     .then(data => {
-      //console.log(data.getRecommendedKr.item.map((it) => it.MAIN_TITLE));
-      setResult(data.getRecommendedKr.item);
+      setResult(data);
       setIsDataLoaded(true);
     })
     .catch(error => console.log(error));
-
-    fetch(gyeongnamUrl)
-    .then(response => response.json())
-    .then(data => {
-      //console.log(data.gyeongnamtourculturelist.body.items.item.map((it) => it.data_title));
-      setRows(data.gyeongnamtourculturelist.body.items.item);
-      setIsRowsLoaded(true);
-    })
-    .catch(error => console.log(error)); 
   },[]);
-  const value=location.pathname==="/list"?rows:result;
 
-  if(!isDataLoaded && !isRowsLoaded){
-    return <div>데이터를 불러오는 중입니다...</div>
+  if(!isDataLoaded){ 
+    return <div>데이터를 불러오는 중입니다...</div>;
   }
-  else{
-  return (
-    <>
-        <stateContext.Provider value={value}>
+
+  else {
+    return (
+      <>
+        <BoardStateContext.Provider value={result}>
           <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/detail/:UC_SEQ' element={<Detail />} />
-            <Route path='/list' element={<List />} />
           </Routes>
-          </stateContext.Provider>
+        </BoardStateContext.Provider>
       </>
-  );
-  };
+    );
+  }
 };
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,49 @@
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import BusantravelList from '../component/BusantravelList';
-import Header from '../component/Header';
+import BoardList from '../board/BoardList';
 
-const Home=()=>{
+// import BusantravelList from '../component/BusantravelList';
+// import Header from '../component/Header';
+
+// const Home=()=>{
+//     return(
+//          <Container>
+//             <Row>
+//                 <Col>
+//                     <Header />
+//                 </Col>
+//             </Row>    
+//             <Row>
+//                 <Col>
+//                     <h2 className='text-center'>부산 테마여행 리스트</h2>
+//                 </Col>
+//             </Row>    
+//             <Row>
+//                 <Col>
+//                     <BusantravelList />
+//                 </Col>
+//             </Row>
+//     </Container>
+//     );
+// };
+
+// export default Home;
+
+const Home = () => {
     return(
-         <Container>
+        <Container>
             <Row>
                 <Col>
-                    <Header />
-                </Col>
-            </Row>    
-            <Row>
-                <Col>
-                    <h2 className='text-center'>부산 테마여행 리스트</h2>
-                </Col>
-            </Row>    
-            <Row>
-                <Col>
-                    <BusantravelList />
+                <h2 className="text-center">게시판 리스트</h2>
                 </Col>
             </Row>
-    </Container>
+            <Row>
+                <Col>
+                    <BoardList />
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
